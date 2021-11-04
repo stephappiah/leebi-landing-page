099fdea0191bfce608250f6711189a6e5b36415a
Stephen Appiah <appstephen8@gmail.com>
2021-11-04 13:25:59 +0000

    initial cleanup & tailwind css setup

---
+++ src/components/Body.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Body extends Component {
+  render() {
+    return (
+      <div>
+        <p className='text-red-700'>
+         Content of the page
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Body;
